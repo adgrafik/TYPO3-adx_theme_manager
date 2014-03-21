0fdc8213502e391c329cb8a81783762dd3587065
Arno Dudek <webmaster@adgrafik.at>
2014-03-21 15:03:54 +0100

    [FEATURE] Added ext_tables.sql support for themes.

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,15 +13,19 @@ if ($themesPath) {
 	$themes = t3lib_div::get_dirs($themesPath);
 	$extensionPath = t3lib_extMgm::extPath($_EXTKEY);
 
-	foreach ($themes as $theme) {
-		require_once($themesPath . $theme . '/ext_localconf.php');
+	foreach ($themes as $themeName) {
+		require_once($themesPath . $themeName . '/ext_localconf.php');
+		// Include ext_tables.sql.
+		if (is_file($themesPath . $themeName . '/ext_tables.sql')) {
+			$GLOBALS['TYPO3_LOADED_EXT'][$_EXTKEY . $themeName]['ext_tables.sql'] = $pathAndFilename;
+		}
 	}
 
 	// Append theme path to allowed paths.
 	$GLOBALS['TYPO3_CONF_VARS']['FE']['addAllowedPaths'] .= ($GLOBALS['TYPO3_CONF_VARS']['FE']['addAllowedPaths'] ? ',' : '') . $extensionConfiguration['theme'];
 }
 
-// Initialize hook
+// Initialize TypoScript template hook.
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tstemplate.php']['includeStaticTypoScriptSourcesAtEnd'][] = 'EXT:adx_theme_manager/Classes/Hooks/TsTemplate.php:&Tx_AdxThemeManager_Hooks_TsTemplate->includeStaticTypoScriptSources';
 
 ?>
