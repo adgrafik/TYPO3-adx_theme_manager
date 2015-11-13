8bd4cffedbca800e5edad2177886d4471406c806
adgrafik <webmaster@adgrafik.at>
2015-11-13 13:49:21 +0100

    [TASK] Changed localization from xml to xlf.

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,7 +3,7 @@ defined('TYPO3_MODE') or die();
 
 $tempColumns = array(
 	'tx_adxthememanager_static_files' => array(
-		'label' => 'LLL:EXT:adx_theme_manager/Resources/Private/Language/Locallang.xml:tx_adxthememanager_static_files',
+		'label' => 'LLL:EXT:adx_theme_manager/Resources/Private/Language/locallang_db.xlf:sys_template.tx_adxthememanager_static_files',
 		'exclude' => 1,
 		'config' => array(
 			'type' => 'select',
